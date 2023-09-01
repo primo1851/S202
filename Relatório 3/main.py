0e65f65d00513fe7e5b414ae08ba4ad0926c5a05
primo1851 <luiza.lp@gec.inatel.br>
2023-08-31 22:46:15 -0300

    refactor: change params

--- Relatório 3/main.py
+++ Relatório 3/main.py
@@ -4,7 +4,7 @@ from writeAJson import writeAJson
 writeAJson(Pokedex().getPokemonsByType("Fire"), "Types")
 writeAJson(Pokedex().getPokemonsBetweenId("19", "51"), "IDs")
 writeAJson(Pokedex().getPokemonsByEggDistance("2 km"), "Eggs")
-writeAJson(Pokedex().getPokemonsByHeight("1.80 m"), "Height")
+writeAJson(Pokedex().getPokemonsByHeight("1.00 m"), "Height")
 writeAJson(
     Pokedex().getPokemonsByPreviousEvolution("004", "Charmander"),
     "PreviousEvolution",
